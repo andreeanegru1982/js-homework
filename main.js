6bdd8e601c0cf27a4184c0cc227abb755e962f86
AndreeaNegru <andreeanegru1982@gmail.com>
2025-03-05 17:57:18 +0200

    corrected errors and started 3rd function

--- main.js
+++ main.js
@@ -14,35 +14,65 @@ their information in an array. (3 points)*/
 let studentNumber = prompt(`How many students are inrolled?`);
     studentNumber = Number(studentNumber);
 
-    let students = [];
-    let firstName = [];
-    let age = [];
-    let attendance = [];
+
+// let firstName = [];
+// let age = [];
+// let attendance = [];
+let students = [];
 
 function getUserInfo(){
-   
+    
     for(let i = 0; i < studentNumber; i++){
-        firstName[i] = prompt(`Input the ${i+1} student's name `);
-        age[i] = Number(prompt(`Input ${firstName[i]}'s age`));
-        attendance[i] = confirm(`Is ${firstName[i]} present?`);
+        let name = prompt(`Input the ${i+1} student's name `);
+        let age = Number(prompt(`Input ${name}'s age`));
+        let attendance = confirm(`Is ${name} present?`);
 
-        students.push({ firstName, age, attendance });
+        students.push({ firstName: name, studAge: age, present: attendance });
     }
 }
 
 getUserInfo();
+
 
 /* Create a function showAttendancePercentage that:
 
 Calculates the percentage of students who are present.
 Displays this percentage in an alert. (2 points)*/
 
-function showAttendancePercentage(students) {
-    const prezStudents = [...attendance];
-    let presentCount = prezStudents.filter(prezStudent => prezStudent).length;
+function showAttendancePercentage() {
+    //const prezStudents = [...students];
+    let presentCount = students.filter(student => student.present).length;
     let percentage = (presentCount / students.length) * 100;
 
     alert(`Today, ${percentage.toFixed(2)}% of students are present.`);
 }
 
-showAttendancePercentage(students); 
+showAttendancePercentage(); 
+
+/*Create a function createTeams that:
+
+Takes a number as a parameter (the desired number of teams).
+
+Forms teams only with present students.
+
+Distributes students across the teams in a structured way so that each team receives students in a cyclic order. (2 points)
+
+(Bonus) Shuffles the students before assigning them to teams to ensure variety. (1 point)
+
+For 10 present students and 3 teams required, logs in the console the teams in the following format:*/
+
+function createTeams(){
+    let teamNo = prompt(`How many teams would you like?`);
+    teamNo = Number(teamNo);
+
+    let presentStudents = students.filter(student => student.present).map(student => student.firstName);  //ar trebui sa ia din students valorile adevarate din attendance si sa creeze un nou vector cu numele corespunzatoare.
+    for(let i= presentStudents.length - 1; i > 0; i--){
+        let s = Math.floor(Math.random() * (i+1));
+        [presentStudents[i], presentStudents[s]] = [presentStudents[s], presentStudents[i]];
+    }
+    return presentStudents;
+
+    let teams = teamNo;
+    
+}
+createTeams();
